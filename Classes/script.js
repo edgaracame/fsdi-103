e0581338307b02b116618cca6bba88f29196963f
edgarcastillo17 <60405064+edgaracame@users.noreply.github.com>
2022-05-15 11:26:28 -0700

    Creating Variables
    Class 1 activities

---
+++ Classes/script.js
@@ -0,0 +1,25 @@
+        /*alert("Hello world!");
+        prompt("Enter tour name:");
+        console.log("This is a message");
+        console.warn("This is a warning message");
+        console.error("This is an error message");*/
+
+        //var x; //Declaration of the variable
+        //x = 10; //Assignation of the variable
+        //document.writeln(x);
+
+        // var userName = "Edgar";
+        // let userAge = 99; //Declaraction of the variable
+        // userAge = 98;
+        // document.writeln(userAge);
+        // document.writeln(`<p>The user name is: ${userName}, and the age is ${userAge}</p>`);
+        
+        // const taxes = 11; // Declaration of the constant
+        // document.writeln(taxes);
+
+        let numberChildren = 3;
+        let partnerName = "Evelyn";
+        let geographicLocation = "United Kingdom";
+        let jobTitle = "veterinary";
+
+        document.writeln(`<p>You will be a ${jobTitle} in ${geographicLocation}, and married to ${partnerName} with ${numberChildren} kids.</p>`)
